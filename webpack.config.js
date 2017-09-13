85365b97d8ab456a454adfd6b9994f20b7bda757
Drogbi <al.ignashevich@gmail.com>
2017-09-13 04:52:04 +0300

    3D_Game/feature: project init

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: "./index.html",
+  filename: './index.html',
+  inject: 'body'
+});
+
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        exclude: '/node_modules/',
+        options: {
+          configFile: 'tslint.json',
+          tsConfigFile: 'tsconfig.json'
+        }
+      }
+    ]
+
+  },
+  resolve: {
+    // you can now require('file') instead of require('file.js')
+    extensions: ['.tsx','.ts', '.js']
+  },
+  plugins: [HTMLWebpackPluginConfig]
+
 };
