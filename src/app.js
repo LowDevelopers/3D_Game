52bb1559ebf95a3f7fb65367897b41f1f14c9746
ALashchetko <flatron_vi@mail.ru>
2017-11-15 16:48:40 +0300

    add map folder and rename "name" in map_1.json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import 'p2';
 import 'pixi';
 import 'phaser';
 import { images } from './images';
+import { levels } from './maps';
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'root', { preload: preload, create: create });
 
 function preload() {
