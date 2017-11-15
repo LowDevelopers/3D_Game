96bc2d7136204375a2ea91740c6cc64de9cf220a
ALashchetko <flatron_vi@mail.ru>
2017-11-15 18:31:18 +0300

    clean version

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ import { levels } from './maps';
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'root', { preload: preload, create: create });
 
 function preload() {
-    game.load.image('logo', images.logo);
+    game.load.tilemap('level1', levels.level1, null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('logo', images.platform1);
 }
 
 function create() {
--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,6 @@
 const images = {
-    logo: require('./logo.png')
+    logo: require('./logo.png'),
+    platform1: require('./platform1.png')
 }
 
 export { images };
